eb8f4e8a2fe956b4062aa6d92f938a8a16795da8
Muskaan Singh <muskaansingh235@gmail.com>
2020-08-06 14:25:59 +0530

    Updates

---
+++ precache-manifest.261968dc0727d8e8d38679ecb34f1ea3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "528037e616e0b3dbd491090c8aed50c9",
+    "url": "/Quiz-App/index.html"
+  },
+  {
+    "revision": "7c44d99e20ca787935db",
+    "url": "/Quiz-App/static/css/main.9128b0dc.chunk.css"
+  },
+  {
+    "revision": "ec14eb5d75d75d7094b0",
+    "url": "/Quiz-App/static/js/2.54fabc67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Quiz-App/static/js/2.54fabc67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c44d99e20ca787935db",
+    "url": "/Quiz-App/static/js/main.b81925f2.chunk.js"
+  },
+  {
+    "revision": "73aab01c2bdaebf7612f",
+    "url": "/Quiz-App/static/js/runtime-main.d7608476.js"
+  }
+]);
